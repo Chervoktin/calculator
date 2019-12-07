8f0fa2f5a15d7700f8c97878242c567c4103d834
Kirill <58602653+Chervoktin@users.noreply.github.com>
2019-12-07 16:02:58 +0300

    added StatesCheck

---
+++ src/com/company/StatesCheck.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public enum StatesCheck {
+    start,
+    stop,
+    reciveNumber,
+    reciveOperators,
+    reciveNameOfFunctions,
+    reciveOpenBracket,
+    reciveCloseBracket,
+    reciveVarible
+
+}
