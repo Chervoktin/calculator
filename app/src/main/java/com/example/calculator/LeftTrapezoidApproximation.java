43297ba75e5a0fac2a1873660b582682b6255bd8
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-05-13 19:55:40 +0300

    added LeftTrapezoidApproximation

---
+++ app/src/main/java/com/example/calculator/LeftTrapezoidApproximation.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+
+public class LeftTrapezoidApproximation {
+    public double calculate(IFunction function, double a, double b, int n) throws InvalidTokenException, FunctionNotFoundException {
+        double delta = (b - a) / n;
+        double x = a + delta;
+        double sum = 0;
+        double f1;
+        double fn;
+        for (int i = 1; i < n; i++) {
+            sum += function.calculate(x);
+            x += delta;
+        }
+        f1 = function.calculate(a);
+        fn = function.calculate(x);
+        sum += (f1 + fn) / 2;
+        sum *= delta;
+        return sum;
+    }
+}
