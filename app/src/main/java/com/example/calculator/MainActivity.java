84d7dfb69926ffc8af6b1bdcb2caaac6a06b2fe1
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-05-15 22:25:40 +0300

    changed MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,22 +15,35 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextStringToParse;
+    EditText editTextA;
+    EditText editTextB;
+    EditText editTextN;
+    TextView textViewIsCheck;
+    Handler handler;
+    TextView textViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextStringToParse = findViewById(R.id.editTextIntegral);
+        editTextA = findViewById(R.id.editTextA);
+        editTextB = findViewById(R.id.editTextB);
+        editTextN = findViewById(R.id.editTextN);
+        textViewResult = findViewById(R.id.textViewResult);
+        final int SHOW_RESULT = 1;
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                switch (msg.what){
+                    case SHOW_RESULT:
+                        textViewResult.setText((String)msg.obj);
+                }
+            }
+        };
 
-        final EditText editTextStringToParse = findViewById(R.id.editTextIntegral);
-        final EditText editTextA = findViewById(R.id.editTextA);
-        final EditText editTextB = findViewById(R.id.editTextB);
-        final EditText editTextN = findViewById(R.id.editTextN);
-
-        final TextView textViewResult = findViewById(R.id.textViewResult);
-        final TextView textViewIsCheck = findViewById(R.id.textView);
-        final Button button = findViewById(R.id.button);
-
-
+        textViewIsCheck = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,23 +52,36 @@ public class MainActivity extends AppCompatActivity {
                     final ArrayList<Token> tokens = Parser.parse(str);
                     textViewIsCheck.setText("");
 
-                    double a = Double.parseDouble(editTextA.getText().toString());
-                    double b = Double.parseDouble(editTextB.getText().toString());
-                    int n = Integer.parseInt(editTextN.getText().toString());
-                    LeftTrapezoidApproximation leftTrapezoidApproximation = new LeftTrapezoidApproximation();
-                    final double result = leftTrapezoidApproximation.calculate(new IFunction() {
+                    final double a = Double.parseDouble(editTextA.getText().toString());
+                    final double b = Double.parseDouble(editTextB.getText().toString());
+                    final int n = Integer.parseInt(editTextN.getText().toString());
+                    final int SHOW_RESULT = 1;
+
+                    Runnable runnable = new Runnable() {
                         @Override
-                        public double calculate(double varible) {
-                            double result = 0;
-                            try{
-                               result = Calculator.calculate(tokens,varible);
-                            }catch (FunctionNotFoundException e){
-                                 textViewIsCheck.setText(e.getMessage());
-                            }
-                            return result;
+                        public void run() {
+                            LeftTrapezoidApproximation leftTrapezoidApproximation = new LeftTrapezoidApproximation();
+                            final double result = leftTrapezoidApproximation.calculate(new IFunction() {
+                                @Override
+                                public double calculate(double varible) {
+                                    double result = 0;
+                                    try{
+                                        result = Calculator.calculate(tokens,varible);
+                                    }catch (FunctionNotFoundException e){
+                          //              textViewIsCheck.setText(e.getMessage());
+                                    }
+                                    return result;
+                                }
+                            },a,b,n);
+                            Message msg = handler.obtainMessage(SHOW_RESULT,Double.toString(result));
+                            handler.sendMessage(msg);
+                            Log.e("test",Double.toString(result));
+                        //    textViewResult.setText(Double.toString(result));
                         }
-                    },a,b,n);
-                    textViewResult.setText(Double.toString(result));
+                    };
+                    Thread th = new Thread(runnable);
+                    th.start();
+
                 } catch (InvalidTokenException e) {
                     textViewIsCheck.setText(e.getMessage());
 
