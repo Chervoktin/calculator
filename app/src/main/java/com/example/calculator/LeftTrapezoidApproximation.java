a8f09be425e93e94fa543d1e0eaf6bcba5a43ae1
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-05-16 17:45:28 +0300

    changed LeftTrapezoidApproximation

--- app/src/main/java/com/example/calculator/LeftTrapezoidApproximation.java
+++ app/src/main/java/com/example/calculator/LeftTrapezoidApproximation.java
@@ -1,15 +1,35 @@
 package com.example.calculator;
 
-import java.util.ArrayList;
+import android.os.Message;
+import android.os.Handler;
+import android.util.Log;
 
 public class LeftTrapezoidApproximation {
+    private Handler handler = null;
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
     public double calculate(IFunction function, double a, double b, int n) {
+        int SHOW_PROGRESS = 3;
         double delta = (b - a) / n;
         double x = a + delta;
         double sum = 0;
         double f1;
         double fn;
+        int per = 0;
+        int prev = 0;
         for (int i = 1; i < n; i++) {
+            prev = per;
+            per = i / (n / 100);
+            if (handler != null) {
+
+                if (per != prev) {
+                    Message msg = handler.obtainMessage(SHOW_PROGRESS, per );
+                    handler.sendMessage(msg);
+                }
+            }
             sum += function.calculate(x);
             x += delta;
         }
@@ -17,6 +37,10 @@ public class LeftTrapezoidApproximation {
         fn = function.calculate(x);
         sum += (f1 + fn) / 2;
         sum *= delta;
+        if(handler != null){
+            Message msg = handler.obtainMessage(SHOW_PROGRESS, 100 );
+            handler.sendMessage(msg);
+        }
         return sum;
     }
 }
